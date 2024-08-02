18b71f7d6a3f41444097a75dc62e197d3a4dba9c
macedorayc <macedorayane335@gmail.com>
2024-08-02 16:13:46 -0300

    atualixado

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-import  express from "express";
+import express from "express";
 import nodemon from "nodemon";
 const servidor = express();
 
-servidor.listen (5001,() => console.log('Api'))
+servidor.listen(5001, () => console.log('API subiu com sucesso na porta 5001'))
 
 
- servidor.get ("/somar/:n1/:n2", (req, resp) =>{
-    let n1 = Number(req.params.n1)
-    let n2 = Number(req.params.n2)
-    let soma = n1+n2
-    resp.send (`A soma dos números é ${soma}`)
- })
+servidor.get("/projeto/somar/:n1/:n2", (req, resp) => {
+   let n1 = Number(req.params.n1)
+   let n2 = Number(req.params.n2)
+   let soma = n1 + n2
+   resp.send(`A soma dos números é ${soma}`)
+})
+
+
+servidor.get("/projeto/subtrai/", (req, resp) => {
+
+   let n1 = Number(req.query.n1)
+   let n2 = Number(req.query.n2)
+   let sub = n1 - n2
+
+   resp.send({
+      sub:sub
+    })
+
+})
