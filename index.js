ad2751c711aaacecfb9c04c0c26dd5f36c0835f7
Mariana <mazzinoob@gmail.com>
2024-08-02 16:22:38 -0300

    oii

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import  express from "express";
-import nodemon from "nodemon";
+
 const servidor = express();
 
 servidor.listen (5001,() => console.log('Api'))
-
 
  servidor.get ("/somar/:n1/:n2", (req, resp) =>{
     let n1 = Number(req.params.n1)
     let n2 = Number(req.params.n2)
     let soma = n1+n2
-    resp.send (`A soma dos números é ${soma}`)
+    resp.send({
+        soma: soma
+    })
  })
+
+ 
